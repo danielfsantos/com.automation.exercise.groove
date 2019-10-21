3f94469697a99e14ac0f29975e2b6ad88cfd8e90
Daniel Fernandes <danielfsantos56@gmail.com>
2019-10-20 22:19:14 -0300

    first commit

---
+++ src/main/java/com/automation/exercise/com/automation/exercise/groove/TestRunner.java
@@ -0,0 +1,8 @@
+package com.automation.exercise.com.automation.exercise.groove;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.testng.AbstractTestNGCucumberTests;
+
+@CucumberOptions(plugin = "json:target/cucumber-report.json")
+public class TestRunner extends AbstractTestNGCucumberTests {
+}
