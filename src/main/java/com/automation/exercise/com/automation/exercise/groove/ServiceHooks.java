98be201fdf95b930c24210f1ff9199a856a90de4
Daniel Fernandes <danielfsantos56@gmail.com>
2019-10-22 23:19:17 -0300

    Inicializar driver

--- src/main/java/com/automation/exercise/com/automation/exercise/groove/ServiceHooks.java
+++ src/main/java/com/automation/exercise/com/automation/exercise/groove/ServiceHooks.java
@@ -1,12 +1,17 @@
 package com.automation.exercise.com.automation.exercise.groove;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 import cucumber.api.java.Before;
  
 
 public class ServiceHooks {
+	
+		
     @Before
     public void initializeTest(){
-        System.out.println("Esta Passando ?");
+    	System.setProperty("webdriver.chrome.driver", "C:\\Users\\Daniel\\eclipse-workspace\\com.automation.exercise.groove\\driver\\chromedriver.exe");
     }
  
 }
--- src/main/java/com/automation/exercise/com/automation/exercise/groove/Steps/TesteStep.java
+++ src/main/java/com/automation/exercise/com/automation/exercise/groove/Steps/TesteStep.java
@@ -7,16 +7,11 @@ public class TesteStep {
 
 	@Dado("^que eu esteja na home$")
 	public void homePage() throws Throwable {
-	   //
-	 //ConfigServices config = new ConfigServices();
-	 //config.initializateSite();
+	
 	}
 
 	@Quando("^clicar em se cadastrar$")
 	public void ClickInRegistry() throws Throwable {
 	 
-		System.out.println("e aqui ?");
 	}
-
-	
 }
